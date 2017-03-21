df377692cb0fc0f2b9822da450c02eab4fb2770d
JonasMel <jome@protonmail.com>
2017-03-21 15:16:09 +0100

    changed func2

--- utils.h
+++ utils.h
@@ -1,4 +1,5 @@
 unsigned char ShuffleBitsA(unsigned char c);
 unsigned char ShuffleBitsB(unsigned char c);
 unsigned char ShuffleBitsC(unsigned char c);
+
 void PrintBufferContents(const unsigned char* buf, int n);
